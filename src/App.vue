e3cdfe0d86bd00839d61da1d6ed47989048ffeb7
Johnson A.Siziba <siziba.uz@gmail.com>
2020-01-28 09:39:41 +0200

    Interface

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
     <v-app>
+        <app-navigation></app-navigation>
+
         <v-content transition="slide-x-transition">
-            <router-view />
+            <router-view></router-view>
         </v-content>
     </v-app>
 </template>
 
 <script>
-export default {
-    name: 'App'
-};
+    import AppNavigation from '@/components/AppNavigation';
+
+    export default {
+        name: 'App',
+        components: {
+            AppNavigation
+        }
+    };
 </script>
 
 <style>
